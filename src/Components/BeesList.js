9937ae42649f613892ec1aa0c131d4b44ea3600e
Augusto <jesus_augga_99@icloud.com>
2022-08-11 17:20:17 -0500

    Add Bees list component

---
+++ src/Components/BeesList.js
@@ -0,0 +1,9 @@
+const BeesList = () => (
+  <ul>
+    <li>Bee 1</li>
+    <li>Bee 2</li>
+    <li>Bee 3</li>
+  </ul>
+);
+
+export default BeesList;
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,9 @@
+import BeesList from '../Components/BeesList';
+
 const HomePage = () => (
   <div>
     <h1>Home Page</h1>
+    <BeesList />
   </div>
 );
 
