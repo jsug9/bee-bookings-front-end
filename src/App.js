6ebea515529fd428327047af7a62ca1f4d4aeb2a
Augusto <jesus_augga_99@icloud.com>
2022-08-11 14:44:23 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import HomePage from './Pages/HomePage';
+import AddReservationPage from './Pages/AddReservationPage';
+import ReservationsPage from './Pages/ReservationsPage';
+import AddBeePage from './Pages/AddBeePage';
+import DeleteBeePage from './Pages/DeleteBeePage';
+
+const App = () => (
+  <Router basename={process.env.PUBLIC_URL}>
+    <Navbar />
+    <Routes>
+      <Route exact path="/" element={<HomePage />} />
+      <Route path="/add_reservation" element={<AddReservationPage />} />
+      <Route path="/reservations" element={<ReservationsPage />} />
+      <Route path="/add_bee" element={<AddBeePage />} />
+      <Route path="/delete_bee" element={<DeleteBeePage />} />
+    </Routes>
+  </Router>
+);
 
 export default App;
