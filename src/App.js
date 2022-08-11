14c9f76fbc9e5a1ba0508c4396da2074ecd682ab
Augusto <jesus_augga_99@icloud.com>
2022-08-11 15:00:35 -0500

    add route for bee details page

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import AddReservationPage from './Pages/AddReservationPage';
 import ReservationsPage from './Pages/ReservationsPage';
 import AddBeePage from './Pages/AddBeePage';
 import DeleteBeePage from './Pages/DeleteBeePage';
+import BeeDetailsPage from './Pages/BeeDetailsPage';
 
 const App = () => (
   <Router basename={process.env.PUBLIC_URL}>
     <Navbar />
     <Routes>
       <Route exact path="/" element={<HomePage />} />
+      <Route exact path="/:bee" element={<BeeDetailsPage />} />
       <Route path="/add_reservation" element={<AddReservationPage />} />
       <Route path="/reservations" element={<ReservationsPage />} />
       <Route path="/add_bee" element={<AddBeePage />} />
---
+++ src/Pages/BeeDetailsPage.js
@@ -0,0 +1,7 @@
+const BeeDetailsPage = () => (
+  <div>
+    <h1>Bee Details Page</h1>
+  </div>
+);
+
+export default BeeDetailsPage;
