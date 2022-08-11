e776355e3a5bef1100ea45e53c75ecb77a5b1d1a
Augusto <jesus_augga_99@icloud.com>
2022-08-11 15:09:52 -0500

    add sample bees

--- src/Redux/bees/BeesReducer.js
+++ src/Redux/bees/BeesReducer.js
@@ -1,6 +1,25 @@
 const GET_BEES = 'Bees/GET_BEES';
 
-const initialState = [];
+const initialState = [
+  {
+    id: 1,
+    name: 'bee1',
+    image: 'https://placekitten.com/200/300',
+    description: 'Awesome bee',
+  },
+  {
+    id: 2,
+    name: 'bee2',
+    image: 'https://placekitten.com/200/300',
+    description: 'Awesome bee',
+  },
+  {
+    id: 3,
+    name: 'bee3',
+    image: 'https://placekitten.com/200/300',
+    description: 'Awesome bee',
+  },
+];
 
 const getBees = async (dispatch) => {
   const payload = ['bee1', 'bee2', 'bee3'];
