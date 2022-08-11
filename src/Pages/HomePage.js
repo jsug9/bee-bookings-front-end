a71234f592d62f2b2e04a03e7868a68be52f75e9
Augusto <jesus_augga_99@icloud.com>
2022-08-11 17:37:01 -0500

    add redux request to get bees

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,22 @@
+import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
+import { getBees } from '../Redux/bees/BeesReducer';
 import BeesList from '../Components/BeesList';
 
-const HomePage = () => (
-  <div>
-    <h1>Our Collection of Bees</h1>
-    <p>Please select a Bee</p>
-    <BeesList />
-  </div>
-);
+const HomePage = () => {
+  const dispatch = useDispatch();
+  const bees = useSelector((state) => state.bees);
+  useEffect(() => {
+    dispatch(getBees());
+  }, []);
+
+  return (
+    <div>
+      <h1>Our Collection of Bees</h1>
+      <p>Please select a Bee</p>
+      <BeesList bees={bees} />
+    </div>
+  );
+};
 
 export default HomePage;
