881774e999a32287bbe9588d68b5acaaa648c016
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-08-11 22:24:06 -0400

    demo form placeholder

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -10,6 +10,31 @@ export const toggleLogin = () => {
 const LoginPage = () => (
   <div id="loginPage" className="invisible">
     <div id="loginClose" role="presentation" onClick={toggleLogin}>X</div>
+    <div className="loginForm mobile">
+      <h1>Login</h1>
+      <section className="loginSection" id="loginSection">
+        <form className="LoginForm" method="post">
+          <input
+            type="text"
+            name="username"
+            id="username"
+            placeholder="Enter your username"
+            required
+            maxLength="30"
+          />
+          {/* <input
+            type="email"
+            name="email"
+            id="mail"
+            placeholder="Email address"
+            title="Please use all lower-case characters for your email address"
+            required
+          /> */}
+          <span className="error" aria-live="polite" />
+          <button type="submit" className="loginBtn">login</button>
+        </form>
+      </section>
+    </div>
   </div>
 );
 
