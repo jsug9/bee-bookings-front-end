16f95649fa8ff82b320022bde3739cb1e5b26426
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-15 15:13:25 -0600

    Sign up reducer created

--- src/Redux/user/UserReducer.js
+++ src/Redux/user/UserReducer.js
@@ -3,10 +3,17 @@ import axios from 'axios';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const signInEndpoint = 'https://bee-store.herokuapp.com/' + 'api/v1/signin';
+const signUpEndpoint = 'https://bee-store.herokuapp.com/' + 'api/v1/signup';
 
 export const signIn = createAsyncThunk('user/signIn', async (username) => {
   const userParams = { user: { username } };
   const { data } = await axios.post(signInEndpoint, userParams);
+  return data;
+});
+
+export const signUp = createAsyncThunk('user/signUp', async (username) => {
+  const userParams = { user: { username } };
+  const { data } = await axios.post(signUpEndpoint, userParams);
   return data;
 });
 
@@ -35,6 +42,17 @@ const userSlice = createSlice({
       state.isLoading = false;
     },
     [signIn.rejected]: (state) => {
+      state.isLoading = false;
+    },
+    [signUp.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [signUp.fulfilled]: (state, action) => {
+      state.username = action.payload.username;
+      state.userId = action.payload.user_id;
+      state.isLoading = false;
+    },
+    [signUp.rejected]: (state) => {
       state.isLoading = false;
     },
   },
