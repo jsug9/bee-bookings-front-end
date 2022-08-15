c30e9530232d8f83a3b488c02ea37bdebb25bda5
Augusto <jesus_augga_99@icloud.com>
2022-08-15 16:56:08 -0500

    import details page scss

--- src/Pages/BeeDetailsPage.js
+++ src/Pages/BeeDetailsPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
-import '../Styles/Homepage.scss';
+import '../Styles/DetailsPage.scss';
 
 const BeeDetailsPage = () => {
   const location = useLocation();
