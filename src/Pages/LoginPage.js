b0ff1bb9ea4c66c183ce74ccbde39d36563b448e
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-15 15:18:10 -0600

    Sign up page functioning

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,8 +1,8 @@
 import '../Styles/login_page.scss';
-import { toggleSignup } from './SignupPage';
-import { signIn } from '../Redux/user/UserReducer';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
+import { toggleSignup } from './SignupPage';
+import { signIn } from '../Redux/user/UserReducer';
 
 export const toggleLogin = () => {
   const loginPage = document.getElementById('loginPage');
@@ -23,33 +23,34 @@ const LoginPage = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(signIn(username));
-  }
+  };
   return (
-  <div id="loginPage" className="invisible">
-    <div id="loginClose" role="presentation" onClick={toggleLogin}>X</div>
-    <div className="loginForm mobile">
-      <h1>Buzz In</h1>
-      <section className="loginSection" id="loginSection">
-        <form className="loginForm" method="post" onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Enter your username"
-            required
-            maxLength="30"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <span className="error" aria-live="polite" />
-          <button type="submit" className="loginBtn">Let&apos;s Go!</button>
-        </form>
-        <div className="signupLink">
-          <p id="signup" role="presentation" onClick={signupToggle}>Sign Up</p>
-        </div>
-      </section>
+    <div id="loginPage" className="invisible">
+      <div id="loginClose" role="presentation" onClick={toggleLogin}>X</div>
+      <div className="loginForm mobile">
+        <h1>Buzz In</h1>
+        <section className="loginSection" id="loginSection">
+          <form className="loginForm" method="post" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Enter your username"
+              required
+              maxLength="30"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <span className="error" aria-live="polite" />
+            <button type="submit" className="loginBtn">Let&apos;s Go!</button>
+          </form>
+          <div className="signupLink">
+            <p id="signup" role="presentation" onClick={signupToggle}>Sign Up</p>
+          </div>
+        </section>
+      </div>
     </div>
-  </div>
-)};
+  );
+};
 
 export default LoginPage;
