8a03ecde7bc089e57b32cabb6c45378e72425f1e
Augusto <jesus_augga_99@icloud.com>
2022-08-15 08:32:29 -0500

    Add hover effects to icons

--- src/Components/BeeItemSocial.js
+++ src/Components/BeeItemSocial.js
@@ -2,9 +2,9 @@ import { Icon } from '@iconify/react';
 
 const BeeItemSocial = () => (
   <div className="flex text-2xl justify-around pb-6 pt-6 pl-3 pr-3 mt-auto w-full">
-    <Icon icon="bi:facebook" />
-    <Icon icon="bi:twitter" />
-    <Icon icon="bi:instagram" />
+    <Icon icon="bi:facebook" className="hover:opacity-50" />
+    <Icon icon="bi:twitter" className="hover:opacity-50" />
+    <Icon icon="bi:instagram" className="hover:opacity-50" />
   </div>
 );
 
