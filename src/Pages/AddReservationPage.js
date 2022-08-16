26ba8d9408284760b2b343b55308509e1b137794
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-16 14:20:00 -0600

    Add state to location in reservation form

--- src/Pages/AddReservationPage.js
+++ src/Pages/AddReservationPage.js
@@ -1,8 +1,11 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addReservation } from '../Redux/reservations/ReservationsReducer';
+import { useLocation } from 'react-router-dom';
 
 const AddReservationPage = () => {
+  const location = useLocation();
+  const { selectedBee } = location.state;
   const [date, setDate] = useState('');
   const [beeId, setBeeId] = useState('');
   const [city, setCity] = useState('');
@@ -26,8 +29,13 @@ const AddReservationPage = () => {
     <div>
       <h1>Add Reservation Page</h1>
       <form method="post" onSubmit={handleSubmit}>
-        <select onChange={(value) => { setBeeId(value); }}>
-          {bees.map((bee) => (<option value={bee.id} key={bee.id.toString()}>{bee.name}</option>))}
+        <select onChange={(e) => { setBeeId(e.target.value); }}>
+          <option value="">Select a Bee</option>
+          {bees.map((bee) => (
+            <option value={bee.id} key={bee.id}>
+              {bee.name}
+            </option>
+          ))}
         </select>
         <input type="date" name="date" id="date" placeholder="Enter date" required value={date} onChange={(e) => setDate(e.target.value)} />
         <input type="text" name="city" id="city" placeholder="Enter city" required value={city} onChange={(e) => setCity(e.target.value)} />
