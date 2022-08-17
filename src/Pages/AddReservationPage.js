20d5567f3143fa125079fd984ed4012716eab59c
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-17 13:46:18 -0600

    Style Reservations form

--- src/Pages/AddReservationPage.js
+++ src/Pages/AddReservationPage.js
@@ -1,14 +1,22 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  Select, MenuItem, InputLabel, FormControl,
+  Select, MenuItem, InputLabel, FormControl, TextField, Button,
 } from '@mui/material';
-import { useLocation } from 'react-router-dom';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { addReservation } from '../Redux/reservations/ReservationsReducer';
+import { getAllBees } from '../Redux/bees/BeesReducer';
+import '../Styles/BeeForm.scss';
 
 const AddReservationPage = () => {
   const location = useLocation();
-  const [date, setDate] = useState('');
+  const navigate = useNavigate();
+  const [date, setDate] = useState(Date.now());
   const [beeId, setBeeId] = useState('');
   const [city, setCity] = useState('');
   const dispatch = useDispatch();
@@ -20,6 +28,7 @@ const AddReservationPage = () => {
       const { bee } = location.state;
       setBeeId(bee.id);
     }
+    dispatch(getAllBees());
   }, []);
 
   const handleSubmit = (e) => {
@@ -31,23 +40,18 @@ const AddReservationPage = () => {
       city,
     };
     dispatch(addReservation(object));
+    navigate('/reservations');
   };
 
   return (
     <div>
-      <h1>Add Reservation Page</h1>
-      <form className="reservation-form" method="post" onSubmit={handleSubmit}>
+      { userId
+      && (
+      <form className="bee-form reservation-form" method="post" onSubmit={handleSubmit}>
+        <h1>Add Reservation Page</h1>
         <FormControl>
           <InputLabel id="bee-label">Bee</InputLabel>
-          <Select labelId="bee-label" label="Bee" style={{ width: '300px' }} value={beeId} onChange={(e) => { setBeeId(e.target.value); }}>
-            { location.state
-          && (
-          <MenuItem value={location.state.bee.id}>
-            {location.state.bee.name}
-          </MenuItem>
-          ) }
-            { !location.state
-          && <MenuItem value="">Select a Bee</MenuItem>}
+          <Select required labelId="bee-label" label="Bee" style={{"margin-bottom": "10px"}} value={beeId} onChange={(e) => { setBeeId(e.target.value); }}>
             {bees.map((bee) => (
               <MenuItem value={bee.id} key={bee.id}>
                 {bee.name}
@@ -55,10 +59,29 @@ const AddReservationPage = () => {
             ))}
           </Select>
         </FormControl>
-        <input type="date" name="date" id="date" placeholder="Enter date" required value={date} onChange={(e) => setDate(e.target.value)} />
-        <input type="text" name="city" id="city" placeholder="Enter city" required value={city} onChange={(e) => setCity(e.target.value)} />
-        <button type="submit">Add Reservation</button>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <MobileDatePicker
+            label="Date"
+            inputFormat="MM/dd/yyyy"
+            value={date}
+            required
+            onChange={(newValue) => setDate(newValue)}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+        <TextField id="city" label="City" variant="outlined" style={{"margin-top": "10px"}} required value={city} onChange={(e) => setCity(e.target.value)} />
+        <Button
+        type="submit"
+        variant="contained"
+        color="success"
+        startIcon={<LibraryAddIcon />}
+        sx={{ fontWeight: 'bold', marginTop: '10px', width: '300px', alignSelf: 'center' }}
+      >
+        Book bee
+      </Button>
       </form>
+      )}
+      {!userId && <h1>Please log in to add a reservation</h1>}
     </div>
   );
 };
