190e9787b7d54a75f4de3f6c778d334a666c1fa7
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-08-16 20:43:58 -0400

    add logic for rendering different views depending on user login status

--- src/Pages/ReservationsPage.js
+++ src/Pages/ReservationsPage.js
@@ -1,7 +1,25 @@
-const ReservationsPage = () => (
-  <div>
-    <h1>Reservations Page</h1>
-  </div>
-);
+import { useSelector } from 'react-redux';
+
+const ReservationsPage = () => {
+  const user = useSelector((state) => state.user);
+
+  if (user.userId) {
+    return (
+      <div>
+        <h1>
+          signed in!
+        </h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h1>
+        {'hi '}
+        {user?.username || ' user'}
+      </h1>
+    </div>
+  );
+};
 
 export default ReservationsPage;
