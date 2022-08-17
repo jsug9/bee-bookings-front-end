0937aceff105de3a7cb34f72412d905311422b8e
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-16 19:35:00 -0600

    State goes from bee details page to add reservations page

--- src/Pages/AddReservationPage.js
+++ src/Pages/AddReservationPage.js
@@ -1,17 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 import { addReservation } from '../Redux/reservations/ReservationsReducer';
-// import { useLocation } from 'react-router-dom';
 
 const AddReservationPage = () => {
-  // const location = useLocation();
-  // const { selectedBee } = location.state;
+  const location = useLocation();
   const [date, setDate] = useState('');
   const [beeId, setBeeId] = useState('');
   const [city, setCity] = useState('');
   const dispatch = useDispatch();
   const userId = useSelector((state) => state.user.userId);
   const bees = useSelector((state) => state.bees.allBees);
+
+  useEffect(() => {
+    if (location.state) {
+      const { bee } = location.state;
+      setBeeId(bee.id);
+    }
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,7 +28,6 @@ const AddReservationPage = () => {
       city,
     };
     dispatch(addReservation(object));
-    console.log(object);
   };
 
   return (
@@ -30,12 +35,24 @@ const AddReservationPage = () => {
       <h1>Add Reservation Page</h1>
       <form method="post" onSubmit={handleSubmit}>
         <select onChange={(e) => { setBeeId(e.target.value); }}>
-          <option value="">Select a Bee</option>
-          {bees.map((bee) => (
-            <option value={bee.id} key={bee.id}>
-              {bee.name}
-            </option>
-          ))}
+          { location.state
+          && (
+          <option value={location.state.bee.id}>
+            {location.state.bee.name}
+          </option>
+          ) }
+          { !location.state
+          && <option value="">Select a Bee</option>}
+          {bees.map((bee) => {
+            if (location.state && (bee.id !== location.state.bee.id)) {
+              return (
+                <option value={bee.id}>
+                  {bee.name}
+                </option>
+              );
+            }
+            return null;
+          })}
         </select>
         <input type="date" name="date" id="date" placeholder="Enter date" required value={date} onChange={(e) => setDate(e.target.value)} />
         <input type="text" name="city" id="city" placeholder="Enter city" required value={city} onChange={(e) => setCity(e.target.value)} />
