e8c9bd1650ec1a7cd4e752b4a79eb83238d7c3e7
Augusto <jesus_augga_99@icloud.com>
2022-08-17 11:42:13 -0500

    update add bee form

--- src/Pages/AddBeePage.js
+++ src/Pages/AddBeePage.js
@@ -1,26 +1,56 @@
+import Box from '@mui/material/Box';
+import { Button, TextField } from '@mui/material';
+import LibraryAddIcon from '@mui/icons-material/LibraryAdd';
 import { useState } from 'react';
 import UploadImage from '../Components/UploadImage';
+import '../Styles/BeeForm.scss';
 
 const AddBeePage = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   return (
-    <div>
-      <h1>Add Bee Page</h1>
-      <form>
-        <label htmlFor="name">
-          Name
-          <input type="text" id="name" />
-        </label>
-        <label htmlFor="description">
-          Description
-          <input type="text" id="description" />
-        </label>
-        <UploadImage
-          selectedFile={selectedFile}
-          setSelectedFile={setSelectedFile}
-        />
-      </form>
-    </div>
+    <form className="bee-form">
+      <h1>Add Your own Bee</h1>
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': {
+            m: 1,
+            display: 'flex',
+            width: '100%',
+            margin: '1rem auto',
+          },
+        }}
+      >
+        <div>
+          <TextField
+            required
+            id="name-form"
+            label="Name"
+          />
+        </div>
+        <div>
+          <TextField
+            required
+            id="description-form"
+            label="Description"
+            multiline
+            rows={4}
+          />
+        </div>
+      </Box>
+      <UploadImage
+        selectedFile={selectedFile}
+        setSelectedFile={setSelectedFile}
+      />
+      <Button
+        variant="contained"
+        color="success"
+        startIcon={<LibraryAddIcon />}
+        sx={{ fontWeight: 'bold' }}
+      >
+        Book bee
+      </Button>
+    </form>
   );
 };
 
