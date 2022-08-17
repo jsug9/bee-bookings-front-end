4b11f34c9d9403ee364479f726d4c3354634ee7a
emyrue <94935633+emyrue@users.noreply.github.com>
2022-08-16 19:12:15 -0600

    Revert "Use mui for select input field"
    This reverts commit d436156decc692236c06e4e6b0458d6174ad9115.

--- src/Pages/AddReservationPage.js
+++ src/Pages/AddReservationPage.js
@@ -1,26 +1,17 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  InputLabel, FormControl, Select, MenuItem,
-} from '@mui/material';
-// import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-// import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { addReservation } from '../Redux/reservations/ReservationsReducer';
-// import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 const AddReservationPage = () => {
-  // const location = useLocation();
+  const location = useLocation();
+  const { selectedBee } = location.state;
   const [date, setDate] = useState('');
   const [beeId, setBeeId] = useState('');
   const [city, setCity] = useState('');
   const dispatch = useDispatch();
   const userId = useSelector((state) => state.user.userId);
   const bees = useSelector((state) => state.bees.allBees);
-  // if (location.state) {
-  //   const { selectedBee } = location.state;
-  //   setBeeId(selectedBee.id);
-  // }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -31,40 +22,25 @@ const AddReservationPage = () => {
       city,
     };
     dispatch(addReservation(object));
+    console.log(object);
   };
 
   return (
     <div>
       <h1>Add Reservation Page</h1>
-      <FormControl fullWidth onSubmit={handleSubmit}>
-        <InputLabel id="bee-select-label">Bee</InputLabel>
-        <Select
-          labelId="bee-select-label"
-          id="bee-select"
-          value={beeId}
-          label="Bee"
-          onChange={(e) => { setBeeId(e.target.value); }}
-          style={{ width: '200px' }}
-        >
+      <form method="post" onSubmit={handleSubmit}>
+        <select onChange={(e) => { setBeeId(e.target.value); }}>
+          <option value="">Select a Bee</option>
           {bees.map((bee) => (
-            <MenuItem value={bee.id} key={bee.id}>
+            <option value={bee.id} key={bee.id}>
               {bee.name}
-            </MenuItem>
+            </option>
           ))}
-        </Select>
+        </select>
         <input type="date" name="date" id="date" placeholder="Enter date" required value={date} onChange={(e) => setDate(e.target.value)} />
-        {/* <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <MobileDatePicker
-          label="Date mobile"
-          inputFormat="MM/dd/yyyy"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-          // renderInput={(params) => <TextField {...params} />}
-        />
-        </LocalizationProvider> */}
         <input type="text" name="city" id="city" placeholder="Enter city" required value={city} onChange={(e) => setCity(e.target.value)} />
         <button type="submit">Add Reservation</button>
-      </FormControl>
+      </form>
     </div>
   );
 };
