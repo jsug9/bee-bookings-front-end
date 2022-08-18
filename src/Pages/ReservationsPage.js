d9001634435320854f86220fe88a3fae2b17d825
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-08-18 13:58:15 -0400

    Add Reservation Page

--- src/Pages/ReservationsPage.js
+++ src/Pages/ReservationsPage.js
@@ -1,21 +1,59 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import BasicTable from '../Components/Table';
+import '../Styles/ReservationsPage.scss';
+import { getReservations } from '../Redux/reservations/ReservationsReducer';
+import { getAllBees } from '../Redux/bees/BeesReducer';
 
 const loginNotice = () => (
-  <div>
+  <div className="center">
     <h1>
       Please sign in to use this page
     </h1>
   </div>
 );
 
+const emptyReservationsNotice = () => (
+  <div className="center">
+    <h1>
+      No Bees Booked 🐝
+    </h1>
+  </div>
+);
+
 const ReservationsPage = () => {
   const user = useSelector((state) => state.user);
+  const bees = useSelector((state) => state.bees.allBees);
+  const reservations = useSelector((state) => state.reservations.allReservations);
 
-  if (user.userId ) {
+  const dispatch = useDispatch();
+
+  const [isEmpty, setIsEmpty] = useState(true);
+
+  useEffect(() => {
+    // checks if bees has been fetched. If not, fetches bees from the database
+    if (bees.length === 0) {
+      dispatch(getAllBees());
+    }
+    dispatch(getReservations(user.userId));
+  }, [user]);
+
+  useEffect(() => {
+    // checks if bees has been fetched. If not, fetches bees from the database
+    if (reservations.length > 0) {
+      setIsEmpty(false);
+    } else {
+      setIsEmpty(true);
+    }
+  }, [reservations]);
+
+  if (user.userId && isEmpty) {
+    return emptyReservationsNotice();
+  }
+
+  if (user.userId) {
     return (
-      <div>
+      <div className="reservations">
         <h1 className="bookTableHeader">
           {'Bees booked by '}
           {user?.username}
