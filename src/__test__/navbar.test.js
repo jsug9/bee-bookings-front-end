4b4b38ddd32544cc306458bb646f4d9902426b0e
Henry Nawrocki <hdev@sztanski.com>
2022-08-19 11:06:58 -0500

    Add and setup msw for mock network tests

--- src/__test__/navbar.test.js
+++ src/__test__/navbar.test.js
@@ -1,16 +1,43 @@
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import axios from 'axios';
 import Navbar from '../Components/Navbar';
 import render, { screen } from './test-utils';
 import userReducer, { logOutUser } from '../Redux/user/UserReducer';
 import user from './__mocks__/userMock';
 
+// src/setupTests.js
+import server from '../mswMocks/server';
+// Establish API mocking before all tests.
+beforeAll(() => server.listen());
+// Reset any request handlers that we may add during the tests,
+// so they don't affect other tests.
+afterEach(() => server.resetHandlers());
+// Clean up after the tests are finished.
+afterAll(() => server.close());
+
+it('Log in a user with axios', async () => {
+  const data = await axios.post(
+    'https://bee-store.herokuapp.com/api/v1/signin',
+    {
+      username: 'AaronIsCool',
+    }
+  );
+  console.log(data);
+  expect(data.data.user_id).toBe(4);
+});
+
+it('deletes an item with axios', async () => {
+  const { data } = await axios.delete(
+    'https://bee-store.herokuapp.com/api/v1/items/6'
+  );
+  expect(data.message).toBe('Item deleted successfully');
+});
+
 it('Navbar renders to the page', () => {
   const tree = render(
     <Router>
       <Navbar />
-    </Router>,
+    </Router>
   );
   expect(tree).toMatchSnapshot();
 });
@@ -19,7 +46,7 @@ it('Checks that the navbar renders its appropriate links', () => {
   render(
     <Router>
       <Navbar />
-    </Router>,
+    </Router>
   );
   expect(screen.getByText(/Home/)).toBeInTheDocument();
 });
@@ -35,7 +62,7 @@ it('updates the mocked rocket reserved status from false to true', async () => {
   render(
     <Router>
       <Navbar />
-    </Router>,
+    </Router>
   );
 
   const finalState = await userReducer(state, logOutUser());
---
+++ src/mswMocks/handlers.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { rest } from 'msw';
+
+export default [
+  rest.post('https://bee-store.herokuapp.com/api/v1/signin', (req, res, ctx) =>
+    res(
+      ctx.status(200),
+      ctx.json({
+        user_id: 4,
+        username: 'AaronIsCool',
+      })
+    )
+  ),
+  rest.delete(
+    'https://bee-store.herokuapp.com/api/v1/items/:id',
+    (req, res, ctx) =>
+      res(
+        ctx.status(200),
+        ctx.json({
+          message: 'Item deleted successfully',
+        })
+      )
+  ),
+];
