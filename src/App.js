45203f6d53dcac6e91d3a70b8825140764458418
Augusto <jesus_augga_99@icloud.com>
2022-08-19 11:52:39 -0500

    update to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import HomePage from './Pages/HomePage';
 import AddReservationPage from './Pages/AddReservationPage';
@@ -10,7 +10,7 @@ import LoginPage from './Pages/LoginPage';
 import SignupPage from './Pages/SignupPage';
 
 const App = () => (
-  <Router basename={process.env.PUBLIC_URL}>
+  <Router>
     <Navbar />
     <LoginPage />
     <SignupPage />
