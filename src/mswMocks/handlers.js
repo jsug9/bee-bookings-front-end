b7883d2de6e218f03caae60da2ca9299af900d4b
Henry Nawrocki <hdev@sztanski.com>
2022-08-19 15:25:13 -0500

    Add mock create bee response

--- src/mswMocks/handlers.js
+++ src/mswMocks/handlers.js
@@ -2,20 +2,41 @@
 import { rest } from 'msw';
 
 export default [
-  rest.post('https://bee-store.herokuapp.com/api/v1/signin', (req, res, ctx) => res(
-    ctx.status(200),
-    ctx.json({
-      user_id: 4,
-      username: 'AaronIsCool',
-    }),
-  )),
-  rest.delete(
-    'https://bee-store.herokuapp.com/api/v1/items/:id',
-    (req, res, ctx) => res(
+  rest.post('https://bee-store.herokuapp.com/api/v1/signin', (req, res, ctx) =>
+    res(
       ctx.status(200),
       ctx.json({
-        message: 'Item deleted successfully',
+        user_id: 4,
+        username: 'AaronIsCool',
       }),
     ),
+  ),
+  // delete bee
+  rest.delete(
+    'https://bee-store.herokuapp.com/api/v1/items/:id',
+    (req, res, ctx) =>
+      res(
+        ctx.status(200),
+        ctx.json({
+          message: 'Item deleted successfully',
+        }),
+      ),
+  ),
+  // create bee
+  rest.post(
+    'https://bee-store.herokuapp.com/api/v1/items/:id',
+    (req, res, ctx) =>
+      res(
+        ctx.status(200),
+        ctx.json({
+          id: 34,
+          name: 'TeeBee',
+          image:
+            'https://previews.123rf.com/images/chudtsankov/chudtsankov1107/chudtsankov110700050/10049766-cute-cartoon-bee.jpg',
+          description: 'A bee that likes to wear tees',
+          created_at: '2022-08-19T20:22:17.746Z',
+          updated_at: '2022-08-19T20:22:17.746Z',
+        }),
+      ),
   ),
 ];
