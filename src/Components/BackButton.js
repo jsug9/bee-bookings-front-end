79666e2625c550895e581353aa83727727c52712
Augusto <jesus_augga_99@icloud.com>
2022-08-23 00:19:44 -0500

    add styling to back button

--- src/Components/BackButton.js
+++ src/Components/BackButton.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Fab } from '@mui/material';
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ChevronLeftOutlinedIcon from '@mui/icons-material/ChevronLeftOutlined';
 
 const BackButton = () => {
   const navigate = useNavigate();
   const redirect = () => navigate('/');
 
-  const backStyle = {
-    position: 'fixed',
-    bottom: 30,
-    left: 25,
-  };
-
   return (
-    <Fab
-      color="inherit"
-      size="large"
-      style={backStyle}
+    <button
+      type="button"
+      className="backButton"
       onClick={redirect}
     >
-      <ArrowBackIosNewIcon />
-    </Fab>
+      <ChevronLeftOutlinedIcon
+        fontSize="large"
+      />
+    </button>
   );
 };
 
